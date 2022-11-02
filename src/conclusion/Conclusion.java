bd6aaa01b45f939dfbf77d3ec8c40c73e56c9e46
Dima <117075948+Dmitri28@users.noreply.github.com>
2022-11-02 11:33:53 +0200

    Conclusion
    Conclusion

---
+++ src/conclusion/Conclusion.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package conclusion;
+
+/**
+ *
+ * @author admin
+ */
+public class Conclusion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=54;
+        int b=7;
+        int c=8;
+        System.out.println(a+" "+b+" "+c);
+    }
+    
+}
